e96ffb25cf83e727599f5108dc3b624a658cc89b
salah-driwa <hamdiessdriwa121@gmail.com>
2024-11-29 14:31:35 +0100

    11/29

--- src/pages/component/ChampionCard.jsx
+++ src/pages/component/ChampionCard.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
 // src/components/ChampionCard.js
-
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { assassinIcon, fighterIcon, mageIcon, marksmanIcon, supportIcon, tankIcon } from "../../assets/icon/icons";
-
 
 const roleIconMap = {
   Marksman: marksmanIcon,
@@ -13,25 +12,34 @@ const roleIconMap = {
   Fighter: fighterIcon,
   Support: supportIcon,
 };
-const ChampionCard = ({ champion }) => {
-  const navigate = useNavigate(); // Initialize the navigate function
-  const roles = champion.role.split('/').map(role => role.trim()); // Split roles
 
-  // Handle card click to navigate to /champion.name
+const ChampionCard = ({ champion }) => {
+  const [isImageLoading, setIsImageLoading] = useState(true); // Loading state for the image
+  const navigate = useNavigate();
+  const roles = champion.role.split("/").map((role) => role.trim());
+
   const handleCardClick = () => {
     navigate(`/${champion.name}`);
   };
+
   return (
-    <div 
-      onClick={handleCardClick} // Add click event to navigate
-      className="border relative min-w-[10vw]  h-fit w-fit hover:bg-opacity-50 bg-slate-400 bg-opacity-25 
+    <div
+      onClick={handleCardClick}
+      className="border relative min-w-[10vw] h-fit w-fit hover:bg-opacity-50 bg-slate-400 bg-opacity-25 
                  border-opacity-25 border-white rounded-lg p-4 shadow-lg hover:shadow-xl cursor-pointer"
     >
-      <div className="pointer-events-none">
-        <img 
-          src={champion.skins[0].image} 
-          alt={champion.name} 
-          className="pointer-events-none object-contain z-10 rounded-t-lg" 
+      <div className="relative pointer-events-none">
+        {isImageLoading && (
+          <div className="  inset-0 h-36 w-64 bg-gray-700 rounded-t-lg animate-pulse"></div>
+        )}
+        <img
+          src={champion.skins[0].image}
+          alt={champion.name}
+          className={`pointer-events-none object-contain z-10 rounded-t-lg ${
+            isImageLoading ? "hidden" : "block"
+          }`}
+          onLoad={() => setIsImageLoading(false)}
+          onError={() => setIsImageLoading(false)} // Ensure loading ends even if there's an error
         />
       </div>
 
@@ -43,17 +51,17 @@ const ChampionCard = ({ champion }) => {
 
         <div className="flex ml-auto gap-4 items-center justify-center mr-3">
           {roles.map((role) => {
-            const roleIcon = roleIconMap[role]; // Get the corresponding icon
+            const roleIcon = roleIconMap[role];
             return roleIcon ? (
-              <div 
-                key={role} 
+              <div
+                key={role}
                 className="relative p-2 overflow-hidden rounded-xl flex items-center justify-center 
                            bg-opacity-80 shadow-lg shadow-black/30 bg-gray-700"
               >
                 <img
                   src={roleIcon}
                   alt={role}
-                  className="w-6 h-6 z-0 brightness-[200] pointer-events-none" 
+                  className="w-6 h-6 z-0 brightness-[200] pointer-events-none"
                 />
               </div>
             ) : null;
